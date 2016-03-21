8918c20bbeb609e323ccf2c79e880134997d701a
CHANG-YU-CHENG <nopony2352676@gmail.com>
2016-03-21 20:51:19 +0800

    improve swap.c

--- swap.c
+++ swap.c
@@ -4,49 +4,69 @@
 typedef struct List_node {
     int value;
     struct List_node *next;
-} List_node;
-
-typedef struct List_node List;
+} List;
 
 List *swap(List *head, List *node_1, List *node_2)
 {
-    if (!head &&
-        (node_1 == NULL) && (node_2 == NULL) &&
+    /*
+    這邊先排除掉三個node其中為0  以及
+    要交換的node相同的情形  有交換和沒交換是一樣的
+    */
+    if ( (head == NULL) ||
+        (node_1 == NULL) || (node_2 == NULL) ||
         (node_1 == node_2))
         return head;
 
-    int num_pre_node_1_and_node_2 = 0;
 
+    /* _head用來暫時紀錄linked list最開頭的node address */
     List *_head = head;
+    
+    /*
+    pre_node_1與pre_node_2用來紀錄在node_1與node_2之前的node
+    tmp_node用來記錄其中一個(node_1 or node_2)的next address
+    */
     List *pre_node_1 = NULL, *pre_node_2 = NULL, *tmp_node;
 
-    while (head && head->next) {
-        if (head->next == node_1) {
-            pre_node_1 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+    if(head == node_1){ // pre_node_1 == NULL  ,  (head != node_2) is always true
+        while (head && head->next) {
+         if (head->next == node_2){ 
+                pre_node_2 = head;
+                break;
+             }          
+            head = head->next;
         }
-
-        if (head->next == node_2) {
-            pre_node_2 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+        head = _head;
+        if(pre_node_2 == NULL) // if node_2 was not found
+            return head;
+   }
+   else if(head == node_2){ // pre_node_2 == NULL  ,  (head != node_1) is always true
+        while (head && head->next) {
+         if (head->next == node_1){
+                pre_node_1 = head;
+                break;
+             }          
+            head = head->next;
+        } 
+        head = _head;
+        if(pre_node_1 == NULL) // if node_1 was not found
+            return head;    
+   }
+   else{ // (head != node_1) and (head != node_2) is always true
+        while (head && head->next) {
+         if (head->next == node_1)
+                pre_node_1 = head;
+            else if(head->next == node_2)
+                pre_node_2 = head;              
+            head = head->next;
+            if(pre_node_1 != NULL && pre_node_2 != NULL)
+                break;
         }
-        head = head->next;
-    }
+        head = _head;
+        if(pre_node_1 == NULL || pre_node_2 == NULL)
+            return head;        
+   }
 
-    head = _head;
-    if (head == node_1) {
-        pre_node_1 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
-    }
-
-    if (head == node_2) {
-        pre_node_2 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
-    }
-
-    if (num_pre_node_1_and_node_2 != 2)
-        return head;
-
+     /*以下兩個if處理node_1 或 node_2為head的情形*/
     if (pre_node_1 == NULL) {
         pre_node_2->next = node_1;
         tmp_node = node_1->next;
@@ -63,19 +83,18 @@ List *swap(List *head, List *node_1, List *node_2)
         return node_1;
     }
 
+    /* 以下兩個if處理node_1 與 node_2相鄰 */
     if (node_2->next == node_1) {
         pre_node_2->next = node_1;
-        tmp_node = node_1->next;
+     node_2->next = node_1->next;
         node_1->next = node_2;
-        node_2->next = tmp_node;
         return head;
     }
 
     if (node_1->next == node_2) {
         pre_node_1->next = node_2;
-        tmp_node = node_2->next;
+        node_1->next = node_2->next;
         node_2->next = node_1;
-        node_1->next = tmp_node;
         return head;
     }
 
